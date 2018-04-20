8fe96af89da1b07dd6e22f312687fcc2f8450e34
Jacob Steves <jgs1998@hotmail.com>
2018-04-19 21:03:56 -0400

    convert to jquery module

---
+++ js/display-if.js
@@ -0,0 +1,33 @@
+(function($) {
+  $('.display-if-selected-option').each(function() {
+      var $targetEl = $(this);
+      var $questionEl = $($targetEl.data('if_selected_option'));
+      var displayIfValueIs = $targetEl.data('selected_value');
+      var displayIfNotValue = $targetEl.data('selected_value_not');
+
+      function showOrHide() {
+          var numChecks = $questionEl.map(function() {
+              var $thisQuestionEl = $(this);
+
+              if (displayIfNotValue) {
+                  return $thisQuestionEl.val() !== displayIfNotValue;
+              } else {
+                  return $thisQuestionEl.val() === displayIfValueIs;
+              }
+          }).toArray().reduce(function(a, b) { return a + b; }, 0);
+
+          if ($questionEl.length > 0 && numChecks == $questionEl.length) {
+              $targetEl.show();
+          } else {
+              $targetEl.hide();
+          }
+
+      }
+
+      $questionEl.on('change', function() {
+          showOrHide();
+      });
+
+      showOrHide();
+  });
+})(jQuery);
